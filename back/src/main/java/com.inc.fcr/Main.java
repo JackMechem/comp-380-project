0f7b35ff4b84145e67b9de7fb8da3976701f4449
RoarkCats <roark007games@gmail.com>
2026-02-25 19:19:17 -0800

    Javalin working + Logger

--- back/src/main/java/com.inc.fcr/Main.java
+++ back/src/main/java/com.inc.fcr/Main.java
@@ -5,9 +5,13 @@ import io.javalin.Javalin;
 public class Main {
     public static void main(String[] args) {
         Javalin j = null;
-//        Object app = Javalin.create(config -> {
+        // Is this broken version maybe newer not older? we're running Javalin 6.7 not 7.x
+//        Javalin app = Javalin.create(config -> {
 //            config.routes.get("/", ctx -> ctx.result("Hello World"));
 //        }).start(7070);
+        Javalin app = Javalin.create(/*config*/)
+        .get("/", ctx -> ctx.result("Hello World"))
+        .start(7070);
 
         System.out.println("Hello from stdout java");
     }
