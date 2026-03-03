2dfbbf3788b76d899be0e66d97d122fb29060a34
Jack Mechem <mechemjack@gmail.com>
2026-03-02 20:04:21 -0800

    Implemented current API to display cars; Created search bar

---
+++ front/app/types/CarTypes.ts
@@ -0,0 +1,18 @@
+export interface Car {
+	vin: string;
+	make: string;
+	model: string;
+	year: number;
+	transmission: string;
+	gears: number;
+	fuelType: string;
+	engineType: string;
+	torqe: number;
+	horsepower: number;
+	seats: number;
+	mpg: number;
+	pricePerDay: number;
+	imageUrls: string[];
+	features: string[];
+	description: string;
+}
