ebff4f1af439374bf5426ee4710a23cded7c0425
Jenny <jenny.ventura.447@my.csun.edu>
2026-03-08 00:33:49 -0800

    Added an ApiController class. Not sure if it goes here or in front folder? It is literally bare bones, so just the file with some comments of what may be needed in it. If it goes somewhere else, sorry. I will add more to it later as well. Thanks.

---
+++ back/src/main/java/com/inc/fcr/ApiController.java
@@ -0,0 +1,12 @@
+public class ApiController {
+
+    // status codes
+    // error handlers that return status codes if can't reach db or improper req or auth or if all went well - 300 maybe add redirects or 404, try-catch 500 if db fails or backend fail
+    // needs to have a few paths - database, server, front website
+    // GET, POST, etc methods, interface in front to interact so admin adds cars - see carcontroller class functs - getallc, createc, getc, updatec, deletec - need to use car objects - some funct need an input passed throun ctx var from javalin - (json style req)
+    // customer reviews done through here
+
+
+
+
+}
