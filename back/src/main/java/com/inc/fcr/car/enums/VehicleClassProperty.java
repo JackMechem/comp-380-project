a4e1efcbc8005fbd7962c10a5f237708f8f517c3
Jenny <jenny.ventura.447@my.csun.edu>
2026-03-09 13:55:45 -0700

    Added electric and changed Van

--- back/src/main/java/com/inc/fcr/car/enums/VehicleClassProperty.java
+++ back/src/main/java/com/inc/fcr/car/enums/VehicleClassProperty.java
@@ -1,5 +1,5 @@
 package com.inc.fcr.car.enums;
 
 public enum VehicleClassProperty {
-    ECONOMY, LUXURY, PERFORMANCE, OFFROAD, VAN
+    ECONOMY, LUXURY, PERFORMANCE, OFFROAD, FULL_SIZE, ELECTRIC
 }
