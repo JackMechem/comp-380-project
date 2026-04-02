a90a1727bc08cdea3449d5a00e8e765d9764fa83
Jenny <jenny.ventura.447@my.csun.edu>
2026-04-01 21:02:25 -0700

    made some changes with some questions

--- back/src/main/java/com/inc/fcr/errorHandling/ApiErrors.java
+++ back/src/main/java/com/inc/fcr/errorHandling/ApiErrors.java
@@ -10,33 +10,21 @@ import com.inc.fcr.car.enums.*;
 
 public final class ApiErrors {
 
-    public static void reDirectError(Context ctx, String message, Exception e) { // 300
-        //ctx.status(302).redirect("/new-location");
+    public static void reDirectError(Context ctx, Exception e) { // 300
         ctx.status(302).header("Location", "/new-location");
     }
 
-    public static void badRequest(Context ctx, String message, Exception e) { // 400
-        //ctx.status(400).json(new ApiErrorResponse(400, message, "" + e, stackTraceString(e))));
+    public static void badRequest(Context ctx, Exception e) { // 400
         ctx.status(400).json(new ApiErrorResponse(400, "Bad Request", "" + e, stackTraceString(e)));
     }
 
-    public static void notFound(Context ctx, String message, Exception e) { // 400
-        //ctx.status(404).json(new ApiErrorResponse(404, message, null, null));
+    public static void notFound(Context ctx) { // 400
         ctx.status(404).json(new ApiErrorResponse(404, "Not Found", null, null));
     }
 
-    public static void serverError(Context ctx, String message, Exception e) { // 500
-        //ctx.status(500).json(new ApiErrorResponse(500, message, "" + e, stackTraceString(e)));
+    public static void serverError(Context ctx, Exception e) { // 500
         ctx.status(500).json(new ApiErrorResponse(500, "Server Error", "" + e, stackTraceString(e)));
     }
-
-    /*could have also just done:
-
-    private static void send(Context ctx, int status, String message, Exception e) {
-        ctx.status(status).json(new ApiErrorResponse(status, message, e != null ? e.toString() : null, e != null ? stackTraceString(e) : null));
-    }
-
-    and then just fill in when the others call on send*/
 
     private static String stackTraceString(Exception e) {
         StringWriter stack = new StringWriter();
