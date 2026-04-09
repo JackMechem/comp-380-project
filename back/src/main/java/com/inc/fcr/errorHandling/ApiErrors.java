dcf882326906fbe4cfdf7b1071a687566a9aae6a
RoarkCats <roark007games@gmail.com>
2026-04-09 14:23:29 -0700

    actually use api error lib

--- back/src/main/java/com/inc/fcr/errorHandling/ApiErrors.java
+++ back/src/main/java/com/inc/fcr/errorHandling/ApiErrors.java
@@ -10,27 +10,27 @@ import com.inc.fcr.car.enums.*;
 
 public final class ApiErrors {
 
-    private static void reDirectError(Context ctx, String newLocation) { // 302
+    public static void redirectError(Context ctx, String newLocation) { // 302
         ctx.status(302).header("Location", newLocation);
     }
 
-    private static void formatError(Context ctx, Exception e) { // 400
+    public static void formatError(Context ctx, Exception e) { // 400
         ctx.status(400).json(new ApiErrorResponse(400, "Improper Object Format", "" + e, stackTraceString(e)));
     }
 
-    private static void queryParamError(Context ctx, Exception e) { // 400
+    public static void queryParamError(Context ctx, Exception e) { // 400
         ctx.status(400).json(new ApiErrorResponse(400, "Invalid Query Parameters", "" + e, stackTraceString(e)));
     }
 
-    private static void notFound(Context ctx) { // 404
+    public static void notFound(Context ctx) { // 404
         ctx.status(404).json(new ApiErrorResponse(404, "Object Not Found", null, null));
     }
 
-    private static void serverError(Context ctx, Exception e) { // 500
+    public static void serverError(Context ctx, Exception e) { // 500
         ctx.status(500).json(new ApiErrorResponse(500, "Server Error", "" + e, stackTraceString(e)));
     }
 
-    private static void databaseError(Context ctx, Exception e) { // 500
+    public static void databaseError(Context ctx, Exception e) { // 500
         ctx.status(500).json(new ApiErrorResponse(500, "Database Error", "" + e, stackTraceString(e)));
     }
 
