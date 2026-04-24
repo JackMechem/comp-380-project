f1646db6b9598e09e684095ce878d712a9b03f68
Jack Mechem <mechemjack@gmail.com>
2026-04-23 17:09:04 -0700

    Deleted old unused fcr-client package

--- fcr-client/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from "./types";
-export { buildQuery, parseResponse, ApiError } from "./core";
-export type { QueryValue } from "./core";
-export { browserApi } from "./browser";
-//# sourceMappingURL=index.d.ts.map
--- fcr-client/src/index.ts
+++
@@ -1,9 +0,0 @@
-// Types
-export * from "./types";
-
-// Utilities
-export { buildQuery, parseResponse, ApiError } from "./core";
-export type { QueryValue } from "./core";
-
-// Browser client (safe for client components)
-export { browserApi } from "./browser";
